9c3b0c7b48d423c44d0c03e764caf1467179da70
lb <liangbing@suomitek.com>
2019-07-29 11:20:51 +0800

    feat: 优化参数配置

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"github.com/ghodss/yaml"
 	"github.com/gin-gonic/gin"
@@ -8,14 +9,18 @@ import (
 	"log"
 )
 
+const VERSION = "v1.0.0"
+
+var configs []Config
+var pathMap = make(map[string]interface{})
+
 type Config struct {
 	Path string      `yaml:"path"`
 	Data interface{} `yaml:"data"`
 }
 
-func main() {
-	var configs []Config
-	yamlFile, err := ioutil.ReadFile("config.yaml")
+func reloadConfigs(configPath string) {
+	yamlFile, err := ioutil.ReadFile(configPath)
 
 	log.Println("yamlFile:", yamlFile)
 	if err != nil {
@@ -26,17 +31,39 @@ func main() {
 		log.Fatalf("Unmarshal: %v", err)
 	}
 
-	r := gin.Default()
-
 	for _, config := range configs {
-		// Serves unicode entities
-		r.GET(config.Path, func(c *gin.Context) {
-			c.PureJSON(200, config.Data)
+		pathMap[config.Path] = config.Data
+	}
+}
+
+func registerURL(configPath string, r *gin.Engine) {
+	reloadConfigs(configPath)
+	for path := range pathMap {
+		r.GET(path, func(c *gin.Context) {
+			reloadConfigs(configPath)
+			c.PureJSON(200, pathMap[path])
 		})
 	}
+}
+
+func main() {
+	var (
+		listenAddress = flag.String("web.listen-address", ":8089", "Address to listen on for web interface and telemetry.")
+		configPath    = flag.String("config", "config.yaml", "the path of config yml file.")
+		showVersion   = flag.Bool("version", false, "Show the version.")
+	)
+
+	flag.Parse()
+	if *showVersion {
+		fmt.Printf("generic-exporter %s", VERSION)
+		return
+	}
+
+	r := gin.Default()
+	registerURL(*configPath, r)
 
 	// listen and serve on 0.0.0.0:8089
-	if err := r.Run(":8089"); err != nil {
+	if err := r.Run(*listenAddress); err != nil {
 		fmt.Println(err)
 	}
 }
